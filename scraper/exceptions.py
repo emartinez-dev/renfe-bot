e55f619874c5c9395b2c4c97270ddc600edce4e2
emartinez <enri.fc.98@gmail.com>
2023-07-19 11:22:38 +0200

    feat(scraper): add logging and custom exceptions

---
+++ scraper/exceptions.py
@@ -0,0 +1,14 @@
+class MaxRetriesExceeded(Exception):
+	pass
+
+class ElementNotFound(Exception):
+	pass
+
+class SearchFailed(Exception):
+	pass
+
+class NoTrainsFound(Exception):
+	pass
+
+class InvalidDate(Exception):
+	pass
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,20 +1,35 @@
+import logging
+import os
 from datetime import datetime, timedelta
 
 import pandas as pd
 from bs4 import BeautifulSoup
 from chromedriver_py import binary_path
 from selenium import webdriver
-from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import (StaleElementReferenceException,
+                                        WebDriverException)
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import WebDriverException
 
+import scraper.exceptions as e
 from scraper.parser import clear_dataframe, parse_table
 from scraper.utils import str_to_dt
+
+if not os.path.exists('logs'):
+	os.makedirs('logs')
+
+logfile = datetime.now().strftime('scraper_%H_%M_%d_%m_%Y.log')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(f'logs/{logfile}', mode='w')
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter('[%(asctime)s] [%(levelname)s]: %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 class RenfeScraper:
 	"""
@@ -42,12 +57,15 @@ class RenfeScraper:
 		try:
 			driver.get(HOME_RENFE)
 			self._wait_for_element(search_selector)
-		except WebDriverException as e:
-			print(f"Error loading Renfe page: {e}")
+			logger.info("Renfe page loaded")
+		except WebDriverException:
+			logger.error(f"Error loading Renfe page")
 		self.df = None
 
 	def find_trains(self, origin_name:str, destination_name:str, origin_date:str, destination_date:str):
+		logger.info(f"Stations: {origin_name} -> {destination_name}")
 		self.search_stations(origin_name, destination_name)
+		logger.info(f"Date: {origin_date} - {destination_date}")
 		self.search_dates(origin_date, destination_date)
 
 	def search_stations(self, origin_name:str, destination_name:str):
@@ -85,7 +103,8 @@ class RenfeScraper:
 
 		# this check should be done before even starting the scraper
 		if ts_destination < ts_origin or dt_origin < dt_yesterday:
-			raise Exception("You can't go back in time")
+			logger.error("Invalid date")
+			raise e.InvalidDate("You can't go back in time")
 
 		# datepicker selectors
 		DATE_OPEN_SELECTOR = "input#first-input"
@@ -105,9 +124,10 @@ class RenfeScraper:
 		train_list_selector = "#listaTrenesTable"
 		try:
 			self._wait_for_element(train_list_selector)
-			print("Trains page loaded")
-		except Exception:
-			print("Trains page couldn't be loaded")
+		except:
+			logger.error("Trains page couldn't be loaded")
+			raise e.SearchFailed("Trains page couldn't be loaded")
+		logger.info("Trains page loaded")
 
 	def get_results(self):
 		driver = self.driver
@@ -128,7 +148,8 @@ class RenfeScraper:
 		vuelta_data = parse_table(vuelta_trains, "vuelta")
 		data = ida_data + vuelta_data
 		if len(data) == 0:
-			raise Exception("No trains found")
+			logger.error("No trains found")
+			raise e.SearchFailed("No trains found")
 		self.df = pd.DataFrame(data)
 		self.df = clear_dataframe(self.df)
 
@@ -148,8 +169,8 @@ class RenfeScraper:
 			if latest_key in train_filter:
 				tickets = tickets[tickets['time_of_departure'] <= train_filter[latest_key]]
 		if len(tickets) == 0:
+			logger.debug(f"No {direction} tickets found. Filter: {train_filter}")
 			return False, None
-		print(f"{direction.capitalize()} tickets found")
 		return True, tickets
 
 	def check_origin_ticket(self, train_filter):
@@ -170,10 +191,17 @@ class RenfeScraper:
 				actions.move_to_element(element).click()
 				actions.perform()
 				break
-			except StaleElementReferenceException:
+			except:
 				num_retries += 1
 				element = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, selector)))
+		if num_retries == MAX_RETRIES:
+			logger.error(f"Max retries exceeded for selector {selector}")
+			raise e.MaxRetriesExceeded(f"Max retries exceeded for selector {selector}")
+
 
 	def _wait_for_element(self, selector):
 		wait = WebDriverWait(self.driver, 10)
 		wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, selector)))
+		if self.driver.find_elements(By.CSS_SELECTOR, selector) == []:
+			logger.error(f"Element with selector {selector} not found")
+			raise e.ElementNotFound(f"Element with selector {selector} not found")

620ec42f7c9dce9bcf736a2e668e8ce8bf5fa943
emartinez <enri.fc.98@gmail.com>
2025-01-31 20:21:28 +0100

    Added the coverage upload to codecov

--- tests/run.py
+++ tests/run.py
@@ -7,10 +7,12 @@ os.environ["PYTHONPATH"] = "src/"
 
 # Run coverage with pytest
 subprocess.run(
-    [sys.executable, "-m", "coverage", "run", "--branch", "--source=src", "-m", "pytest", "tests", "--ignore=tests/integration"],
+    [sys.executable, "-m", "coverage", "run", "--branch", "--source=src", "-m", "pytest", "tests",
+        "--ignore=tests/integration",],
     check=True
 )
 
 # Show coverage report
 subprocess.run([sys.executable, "-m", "coverage", "report"], check=True)
 subprocess.run([sys.executable, "-m", "coverage", "html"], check=True)
+subprocess.run([sys.executable, "-m", "coverage", "xml"], check=True)
